2a7bb428ec62aa60d7d74bceed3af350b048f643
bibek977 <bhattaraibibek91@gmail.com>
2024-06-15 21:38:15 +0545

    readme file edited

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <p>This is a About page.</p>
+
+      This project is developed as sample interview process test by <b>Bibek Bhattarai</b>.
+      You can see source code in <b>git@github.com:bibek977/event_management.git</b>.
+      You can contact me through email : <b>bhattaraibibek91@gmail.com</b>.
+      </div>
   )
 }
 
